66862853d2d219ef13d7268fb62d0de9b43227b9
“theoioannou225@gmail.com” <theoioannou225@gmail.com>
2024-02-15 22:21:51 +0000

    Dashboard Page-flexbox

--- App.js
+++ App.js
@@ -1,71 +1,82 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-
+import { Button, TouchableOpacity, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
 
 import DashboardScreen from './screens/DashboardScreen';
 import MyRunningScreen from './screens/MyRunningScreen';
-import MyGoalsScreen from './screens/MyGoalsScreen'; 
+import MyGoalsScreen from './screens/MyGoalsScreen';
 import MyRewardsScreen from './screens/MyRewardsScreen';
-import MyWaterIntakeScreen from './screens/MyWaterIntakeScreen'; 
+import MyWaterIntakeScreen from './screens/MyWaterIntakeScreen';
 
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+    screenOptions={{
+    headerLeft: () => (
+      <TouchableOpacity  activeOpacity={0.5}>
+        <Image
+          source={require('./icons/logo.png')}
+        />
+      </TouchableOpacity>
+    ),}}
+    >
       <Drawer.Navigator
-      screenOptions={{
-        headerStyle: {backgroundColor: '#3c0a6b'},
-        headerTintColor: 'white',
-        drawerActiveBackgroundColor: '#f00e1ff',
-        drawerActiveTintColor: '#3c0a6b',
-        drawerStyle:{backgroundColor: '#ccc'},
-      }}
+        screenOptions={{
+          headerStyle: { backgroundColor: '#3c0a6b' },
+          headerTintColor: 'white',
+          drawerActiveBackgroundColor: '#f00e1ff',
+          drawerActiveTintColor: '#3c0a6b',
+          drawerStyle: { backgroundColor: '#ccc' },
+         
+          }
+        }
       >
-        <Drawer.Screen 
-        name="Dashboard" 
-        component={DashboardScreen} 
-        options={{
-          drawerIcon: ({color, size}) => 
-          <Ionicons name= 'bar-chart-outline' color={color} size={size}/>
-        }} />
+        <Drawer.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{
+            drawerIcon: ({ color, size }) =>
+              <Ionicons name='bar-chart-outline' color={color} size={size} />
+          }} />
 
-      <Drawer.Screen 
-        name="MyGoals" 
-        component={MyGoalsScreen} 
-        options={{
-          drawerIcon: ({color, size}) => 
-          <Ionicons name= 'create-outline' color={color} size={size}/>
-        }} />
+        <Drawer.Screen
+          name="MyGoals"
+          component={MyGoalsScreen}
+          options={{
+            drawerIcon: ({ color, size }) =>
+              <Ionicons name='create-outline' color={color} size={size} />
+          }} />
 
 
-<Drawer.Screen 
-        name="MyRewards" 
-        component={MyRewardsScreen} 
-        options={{
-          drawerIcon: ({color, size}) => 
-          <Ionicons name= 'ribbon-outline' color={color} size={size}/>
-        }} />
+        <Drawer.Screen
+          name="MyRewards"
+          component={MyRewardsScreen}
+          options={{
+            drawerIcon: ({ color, size }) =>
+              <Ionicons name='ribbon-outline' color={color} size={size} />
+          }} />
 
-<Drawer.Screen 
-        name="MyRunning" 
-        component={MyRunningScreen} 
-        options={{
-          drawerIcon: ({color, size}) => 
-          <Ionicons name= 'walk-outline' color={color} size={size}/>
-        }} />
+        <Drawer.Screen
+          name="MyRunning"
+          component={MyRunningScreen}
+          options={{
+            drawerIcon: ({ color, size }) =>
+              <Ionicons name='walk-outline' color={color} size={size} />
+          }} />
 
-<Drawer.Screen 
-        name="MyWaterIntake" 
-        component={MyWaterIntakeScreen} 
-        options={{
-          drawerIcon: ({color, size}) => 
-          <Ionicons name= 'water-outline' color={color} size={size}/>
-        }} />
+        <Drawer.Screen
+          name="MyWaterIntake"
+          component={MyWaterIntakeScreen}
+          options={{
+            drawerIcon: ({ color, size }) =>
+              <Ionicons name='water-outline' color={color} size={size} />
+          }} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,20 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const DashboardScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Dashboard Screen</Text>
-      {/* Add components for fitness metrics */}
-    </View>
+    
+    <ScrollView contentContainerStyle={styles.container}>
+
+<View style={[styles.sectionContainer, { backgroundColor: '#EF583F' }]}>
+        <Text style={styles.sectionTitle}><Icon name="check" size={20} color="white" /> My Goals</Text>
+        {/* Add components for my goals */}
+      </View>
+
+      <View style={[styles.sectionContainer, { backgroundColor: '#005425' }]}>
+        <Text style={styles.sectionTitle}>        <Icon name="fas fa-running" size={30} color="white" />
+ Running Goals</Text>
+        {/* Add components for running goals */}
+      </View>
+
+      <View style={[styles.sectionContainer, { backgroundColor: '#0089FF' }]}>
+        
+        <Text style={styles.sectionTitle}> <Icon name="tint" size={18} color="white" /> Water Intake</Text>
+        {/* Add components for water intake */}
+      </View>
+
+      <View style={[styles.sectionContainer, { backgroundColor: '#4C0096', marginBottom: 20 }]}>
+        <Text style={styles.sectionTitle}><Icon name="star" size={18} color="white" /> Badges</Text>
+        {/* Add components for badges */}
+      </View>
+
+      
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  sectionContainer: {
+    flex: 1,
+    width: '80%', // Adjust width as needed
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
 
