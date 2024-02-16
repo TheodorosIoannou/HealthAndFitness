41afcaf1d22d0ab0acd828285c7833336164e7aa
“theoioannou225@gmail.com” <theoioannou225@gmail.com>
2024-02-16 11:52:34 +0000

    Dashboard finished as a prototype

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,35 +1,67 @@
 import React from 'react';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const DashboardScreen = () => {
   return (
-    
     <ScrollView contentContainerStyle={styles.container}>
-
-<View style={[styles.sectionContainer, { backgroundColor: '#EF583F' }]}>
-        <Text style={styles.sectionTitle}><Icon name="check" size={20} color="white" /> My Goals</Text>
+      <View style={[styles.sectionContainer, { backgroundColor: '#EF583F' }]}>
+        <View style={styles.iconContainer}>
+          <Icon name="check" size={40} color="white" />
+        </View>
         {/* Add components for my goals */}
+          <Text style={styles.sectionTitle}>Goals</Text>
+        <Text style={styles.sectionBody}>Running:</Text>
+        <Text style={styles.sectionBody}>15/35 km</Text>
+        <Text></Text>
+        <Text style={styles.sectionBody}>Water Intake:</Text>
+        <Text style={styles.sectionBody}>50% Till now</Text>
+        <Text></Text>
+        <Text style={styles.sectionBody}>This section provides to the user Goals details</Text>
       </View>
 
       <View style={[styles.sectionContainer, { backgroundColor: '#005425' }]}>
-        <Text style={styles.sectionTitle}>        <Icon name="fas fa-running" size={30} color="white" />
- Running Goals</Text>
+        <View style={styles.iconContainer}>
+          <Icon name="running" size={40} color="white" />
+        </View>
         {/* Add components for running goals */}
+        <Text style={styles.sectionTitle}>Running</Text>
+        <Text style={styles.sectionBody}>Total Distance Run:</Text>
+        <Text style={styles.sectionTitle}>35 km</Text>
+        <Text></Text>
+        <Text style={styles.sectionBody}>This section provides to the user running details</Text>
       </View>
 
       <View style={[styles.sectionContainer, { backgroundColor: '#0089FF' }]}>
+        <View style={styles.iconContainer}>
+          <Icon name="tint" size={40} color="white" />
+        </View>
+        {/* Add components for water intake */}        
+        <Text style={styles.sectionTitle}>Water Intake</Text>
+        <Text style={styles.sectionBody}>Total Water Consumed:</Text>
+        <Text style={styles.sectionTitle}>200 L</Text>
+        <Text></Text>
+        <Text style={styles.sectionBody}>This section provides to the user running details</Text>
+      </View>
+
+      <View
+        style={[
+          styles.sectionContainer,
+          { backgroundColor: '#4C0096', marginBottom: 20 },
+        ]}
+      >
+        <View style={styles.iconContainer}>
+          <Icon name="medal" size={40} color="white" />
+        </View>
         
-        <Text style={styles.sectionTitle}> <Icon name="tint" size={18} color="white" /> Water Intake</Text>
-        {/* Add components for water intake */}
-      </View>
-
-      <View style={[styles.sectionContainer, { backgroundColor: '#4C0096', marginBottom: 20 }]}>
-        <Text style={styles.sectionTitle}><Icon name="star" size={18} color="white" /> Badges</Text>
         {/* Add components for badges */}
+        <Text style={styles.sectionTitle}>Rewards</Text>
+        <Text style={styles.sectionBody}>Received the: </Text>
+        <Text style={styles.sectionTitle}>10k badge</Text>
+        <Text></Text>
+        <Text style={styles.sectionBody}>This section provides to the user rewards details</Text>
       </View>
-
-      
     </ScrollView>
   );
 };
@@ -39,20 +71,30 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
+   
   },
   sectionContainer: {
     flex: 1,
     width: '80%', // Adjust width as needed
     marginTop: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'left',
+    alignItems: 'left',
     borderRadius: 10,
+    paddingLeft: 20,
+    paddingTop: 20
+  },
+  iconContainer: {
+    marginBottom: 10, // Adjust the margin as needed
   },
   sectionTitle: {
     fontSize: 18,
     fontWeight: 'bold',
     color: 'white',
   },
+  sectionBody: {
+    fontSize: 12,
+    color: 'white',
+  }
 });
 
 export default DashboardScreen;
