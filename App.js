a047cf0a33c8f87cd05f088cbac0bb8d77180e2c
“theoioannou225@gmail.com” <theoioannou225@gmail.com>
2024-02-17 20:36:01 +0000

    Inserted the logo and the user icon

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { Button, TouchableOpacity, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Ionicons } from '@expo/vector-icons';
 import '@fortawesome/fontawesome-free/css/all.css';
-
 
 import DashboardScreen from './screens/DashboardScreen';
 import MyRunningScreen from './screens/MyRunningScreen';
@@ -13,22 +13,46 @@ import MyGoalsScreen from './screens/MyGoalsScreen';
 import MyRewardsScreen from './screens/MyRewardsScreen';
 import MyWaterIntakeScreen from './screens/MyWaterIntakeScreen';
 
-
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
-export default function App() {
+function DashboardStack() {
   return (
-    <NavigationContainer
+    <Stack.Navigator
       screenOptions={{
         headerLeft: () => (
           <TouchableOpacity activeOpacity={0.5}>
-            <Image
-              source={require('./icons/logo.png')}
-            />
+            <Image source={require('./icons/logo.png')} />
           </TouchableOpacity>
         ),
+        headerStyle: { backgroundColor: 'white' },
+        headerTintColor: 'white',
       }}
     >
+      <Stack.Screen
+        name="Dashboard"
+        component={DashboardScreen}
+        options={{
+          headerShown: true, // You can set this to false if you don't want the header in the Dashboard screen
+          headerLeft: () => (
+            <TouchableOpacity activeOpacity={0.5}>
+              <Image source={require('./icons/logo.png')} />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity activeOpacity={0.5}>
+                       <Ionicons name="person-circle-outline" size={40}  />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
       <Drawer.Navigator
         screenOptions={{
           headerStyle: { backgroundColor: '#7DD600' },
@@ -36,50 +60,57 @@ export default function App() {
           drawerActiveBackgroundColor: '#f00e1ff',
           drawerActiveTintColor: '#497D00',
           drawerStyle: { backgroundColor: '#ccc' },
-
-        }
-        }
+        }}
       >
         <Drawer.Screen
           name="Dashboard"
-          component={DashboardScreen}
+          component={DashboardStack}
           options={{
-            drawerIcon: ({ color, size }) =>
-              <Ionicons name='bar-chart-outline' color={color} size={size} />
-          }} />
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="bar-chart-outline" color={color} size={size} />
+            ),
+          }}
+        />
 
         <Drawer.Screen
           name="MyGoals"
           component={MyGoalsScreen}
           options={{
-            drawerIcon: ({ color, size }) =>
-              <Ionicons name='create-outline' color={color} size={size} />
-          }} />
-
-
-        <Drawer.Screen
-          name="MyRewards"
-          component={MyRewardsScreen}
-          options={{
-            drawerIcon: ({ color, size }) =>
-              <Ionicons name='ribbon-outline' color={color} size={size} />
-          }} />
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="create-outline" color={color} size={size} />
+            ),
+          }}
+        />
 
         <Drawer.Screen
           name="MyRunning"
           component={MyRunningScreen}
           options={{
-            drawerIcon: ({ color, size }) =>
-              <Ionicons name='walk-outline' color={color} size={size} />
-          }} />
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="walk-outline" color={color} size={size} />
+            ),
+          }}
+        />
 
         <Drawer.Screen
           name="MyWaterIntake"
           component={MyWaterIntakeScreen}
           options={{
-            drawerIcon: ({ color, size }) =>
-              <Ionicons name='water-outline' color={color} size={size} />
-          }} />
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="water-outline" color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Drawer.Screen
+          name="MyRewards"
+          component={MyRewardsScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="ribbon-outline" color={color} size={size} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
