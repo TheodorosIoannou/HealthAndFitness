94c3a1eca74a2c611f09d5c6a14ce455fac9f4a4
“theoioannou225@gmail.com” <theoioannou225@gmail.com>
2024-02-25 17:43:17 +0000

    Fixed Rewards Page

--- screens/MyRewardsScreen.js
+++ screens/MyRewardsScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView ,Image} from 'react-native';
-import DatePicker from 'react-native-datepicker';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 
@@ -11,6 +11,30 @@ const RunningBadge = ({ name, icon, description }) => (
     <Text style={styles.badgeDescription}>{description}</Text>
   </View>
 );
+
+const BadgeSlider = ({ badges }) => (
+  <SwiperFlatList
+    autoplay
+    autoplayDelay={10}
+    autoplayLoop
+    index={0}
+    showPagination
+  >
+    {badges.map((badge, index) => (
+      <View key={index} style={styles.slide}>
+       {/* Add borderRadius style to Image components */}
+       {React.isValidElement(badge) ? (
+          React.cloneElement(badge, {
+            style: [badge.props.style, { borderRadius: 8 }],
+          })
+        ) : (
+          badge
+        )}
+      </View>
+    ))}
+  </SwiperFlatList>
+);
+
 const AwardedBadgesScreen = () => {
   const [runningGoal, setRunningGoal] = useState('');
   const [waterIntakeGoal, setWaterIntakeGoal] = useState('');
@@ -26,60 +50,88 @@ const AwardedBadgesScreen = () => {
 
   return (
     <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <Text style={styles.title}> <Icon name="tint" size={20} color="black" /> My Rewards</Text>
+      <Text style={styles.title}>
+        {' '}
+        <Icon name="gift" size={20} color="black" /> My Rewards
+      </Text>
       {/* Running Badges Section */}
       <View style={[styles.sectionContainer, styles.greenBackground]}>
-        <Text style={styles.heading}> <Icon name="running" size={20} color="white" /> Running Badges Available</Text>
-        <View style={styles.goalSection}>
-          {/* Add running badges or relevant components here */}
-          
-          <RunningBadge name="5K Champion" icon="medal" description="Complete a 5km run" />
-          <Image source={require('../icons/5kmRun.png')} style={{ alignSelf: 'center' }}/>
-          <RunningBadge name="10K Master" icon="medal" description="Complete 10km run" />
-          <Image source={require('../icons/10kmRun.png')} style={{ alignSelf: 'center' }}/>
-          <RunningBadge name="10K Master" icon="medal" description="Complete 10km run" />
-          <Image style={{ justifyContent: 'center', alignItems: 'center' }} source={require('../icons/15kmRun.png')} style={{ alignSelf: 'center' }}/>
+        <Text style={styles.heading}>
+          {' '}
+          <Icon name="running" size={20} color="white" /> Running Badges Available
+        </Text>
 
-        </View>
+        {/* Add running badges or relevant components here */}
+        <BadgeSlider
+          badges={[
+            <RunningBadge name="5K Beginner" icon="medal" description="Complete a 5km run" />,
+            <Image source={require('../icons/5kmRun.png')} />,
+            <RunningBadge name="10K Pro" icon="medal" description="Complete 10km run" />,
+            <Image source={require('../icons/10kmRun.png')} style={{ alignSelf: 'center' }} />,
+            <RunningBadge name="10K Master" icon="medal" description="Complete 10km run" />,
+            <Image source={require('../icons/15kmRun.png')} style={{ alignSelf: 'center' }} />,
+          ]}
+        />
+
       </View>
       {/* Water Intake Badges Section */}
       <View style={[styles.sectionContainer, styles.lightBlueBackground]}>
-        <Text style={styles.heading}> <Icon name="tint" size={20} color="white" /> Water Intake Badges Available</Text>
-        <View style={styles.goalSection}>
-          {/* Add water intake badges or relevant components here */}
-          
-          <RunningBadge  name="Hydration Beginer" icon="medal" description="Reache 100L water intake goal to gain this " />
-          <Image source={require('../icons/100LWaterIntake.png')} style={{ alignSelf: 'center' }}/>
-         
-          <RunningBadge name="Hydration Pro" icon="medal" description="Achieved 10000ml water intake goal" />
-          <Image source={require('../icons/200LWaterIntake.png')} style={{ alignSelf: 'center' }}/>
-          
-          <RunningBadge name="Hydration Master" icon="medal" description="Exceeded 15000ml water intake goal" />
-          <Image source={require('../icons/300LWaterIntake.jpg')} style={{ alignSelf: 'center' }}/>
-        </View>
+        <Text style={styles.heading}>
+          {' '}
+          <Icon name="tint" size={20} color="white" /> Water Intake Badges Available
+        </Text>
+
+        {/* Add water intake badges or relevant components here */}
+        <BadgeSlider
+          badges={[
+            <RunningBadge name="Beginner" icon="medal" description="Reach 100L water int" />,
+            <Image source={require('../icons/100LWaterIntake.png')} style={{ alignSelf: 'center' }} />,
+            <RunningBadge name="Pro" icon="medal" description="Reach 200L water int" />,
+            <Image source={require('../icons/200LWaterIntake.png')} style={{ alignSelf: 'center' }} />,
+            <RunningBadge name=" Master" icon="medal" description="Reach 300L water int" />,
+            <Image source={require('../icons/300LWaterIntake.jpg')} style={{ alignSelf: 'center' }} />,
+          ]}
+        />
       </View>
 
       {/* Analysis Section */}
-      <View style={[styles.goalSection, styles.grayBackground]}>
-        <Text style={[styles.heading, { textAlign: 'center', textDecorationLine: 'underline' }]}>Analysis</Text>
-        <Text style={[styles.heading, { textAlign: 'left', }]}> This week</Text>
-        <View style={styles.analysisContainer}>
-          <Text style={styles.body}>Running Goal: </Text>
-          <View style={styles.iconContainer}>
-            <Icon name="chart-bar" size={40} color="white" />
-            <Icon name="running" size={40} color="white" />
-          </View>
-        </View>
+      <View style={[styles.sectionContainer, styles.grayBackground]}>
+        <Text style={[styles.heading, { textAlign: 'center', alignSelf: 'center', textDecorationLine: 'underline' }]}>Analysis</Text>
+        <Text style={[styles.heading, { textAlign: 'left', alignSelf: 'left' }]}>Running Badges Awarded </Text>
+        {/* Add running and water intake badges gained */}
+        <BadgeSlider
+          badges={[
+            <RunningBadge name="5K Beginner" icon="medal" description="Complete a 5km run" />,
+            <Image source={require('../icons/5kmRun.png')} />,
+            <RunningBadge name="10K Pro" icon="medal" description="Complete 10km run" />,
+            <Image source={require('../icons/10kmRun.png')} style={{ alignSelf: 'center' }} />,
+          ]}
+        />
+        <Text style={[styles.heading, { textAlign: 'left', alignSelf: 'left' }]}>Water Intake Badges Awarded </Text>
+        {/* Add water intake badges gained */}
+        <BadgeSlider
+          badges={[
+            <RunningBadge name="Beginner" icon="medal" description="Reach 100L water int" />,
+            <Image source={require('../icons/100LWaterIntake.png')} style={{ alignSelf: 'center' }} />,
+            <RunningBadge name="Pro" icon="medal" description="Reach 200L water int" />,
+            <Image source={require('../icons/200LWaterIntake.png')} style={{ alignSelf: 'center' }} />,
+          ]}
+        />
       </View>
     </ScrollView>
-
-  );  
-
+  );
 }
 
 const styles = StyleSheet.create({
+  slide: {
+    flex: 1,
+    justifyContent: 'left',
+    alignItems: 'left',
+
+  },
+
   title: {
-    marginTop:0,
+    marginTop: 0,
     color: 'black',
     fontSize: 18,
     fontWeight: 'bold',
@@ -91,16 +143,14 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flexGrow: 1,
- 
+
     padding: 20,
   },
   sectionContainer: {
     marginBottom: 20,
     padding: 20,
     borderRadius: 8,
-    width: '100%', 
-   
-  
+    width: '100%',
   },
   goalSection: {
     marginTop: 10,
@@ -119,15 +169,16 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
+    marginTop: 10,
   },
   body: {
     color: 'white',
     fontSize: 14,
-    marginBottom: 10,
+    marginBottom: 0,
   },
   analysisContainer: {
     flexDirection: 'row', // Make it a horizontal flexbox
-    
+
     marginBottom: 10, // Adjust as needed
   },
   iconContainer: {
@@ -137,9 +188,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   badgeContainer: {
-    flexDirection: 'column',
-    alignItems: 'center',
+    flexDirection: 'column', // Change to row
+
+    justifyContent: 'space-between', // Add spacing between badges
     marginVertical: 10,
+    paddingHorizontal: 20, // Add padding to each badge for spacing
   },
   badgeText: {
     color: 'white',
